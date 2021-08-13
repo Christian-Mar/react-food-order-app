fa8e52def094d56af4cc626091585b0dfbf7676b
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-13 21:04:18 +0200

    Cart & Modal State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
+// We gebruiken hier de useState omdat we hier de 'Cart' renderen en 
+// dan ook de visibility ervan managen. 
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-    <Cart/>
-      <Header />
+    {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
        <Meals></Meals>
       </main>
